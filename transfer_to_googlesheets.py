e7bd73c2107eb8bb295d2b754b4a4f346c597d30
hsh121 <huan.ham21@imperial.ac.uk>
2023-09-30 18:08:46 +0100

    Remove old code in transfer_to_googlesheets

--- transfer_to_googlesheets.py
+++ transfer_to_googlesheets.py
@@ -1,28 +1,7 @@
-import gspread
-from google.oauth2.service_account import Credentials
-import json
-
-
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
-
-# define keyfile_dict here
-keyfile_dict = {}
-with open("creds.json") as creds_json:
-   keyfile_dict = json.load(creds_json)
-
-creds = Credentials.from_service_account_info(keyfile_dict, scopes=scope)
-
-client = gspread.authorize(creds)
-
-sheet = client.open("tickettesting").sheet1
-
-
-def transfer(customers):
+def transfer(customers, sheet):
   for customer in customers:
       name = str(customer.firstname) + " " + str(customer.surname)
       email = customer.email
       orNum = customer.ordernum
       quantity = int(customer.quantity)
       sheet.append_row([name, email, orNum, quantity, 0])
-
-# transfer("ticlet-qr-2.csv")
