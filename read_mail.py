baa3a6709921da8b720df5b4eadc0c069978142d
hsh121 <huan.ham21@imperial.ac.uk>
2023-10-03 09:19:47 +0100

    Move email title img url to .env

--- read_mail.py
+++ read_mail.py
@@ -1,10 +1,12 @@
 import re
 import json
+import os
 
 
 def read_mail(firstname, surname, quantity, product):
     replacements = compile_replacements(firstname, surname, quantity)
     email = read_content(replacements)
+    email = replace_title_img(email)
     return email
 
 
@@ -12,11 +14,15 @@ def read_content(replacements):
     content = open("email_templates/content.txt").read()
     content = re.sub(r"[\n\t]*", "", content)
     email = open("email_templates/email.html").read()
-    new_email = email.replace("{content}", content)
+    new_email = email.replace("{CONTENT}", content)
 
     for tag in replacements:
         new_email = new_email.replace("{" + tag + "}", replacements[tag])
     return new_email
+
+def replace_title_img(email):
+    title_img_url = os.getenv("EMAIL_TITLE_IMG", default="#")
+    return email.replace("{EMAIL_TITLE_IMG}", title_img_url)
 
 
 def compile_replacements(firstname, surname, quantity):
